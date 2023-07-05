2d1a5f1a4608a96099193e93361c9809d0328654
StephanKragten <72765452+pp007x@users.noreply.github.com>
2023-07-05 18:06:06 +0200

    ugh

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,9 @@
 let API_BASE_URL = "";
 
 
-// API_BASE_URL = "http://localhost:5162";
+API_BASE_URL = "http://localhost:5162";
 
-API_BASE_URL = "https://temptestedwin.azurewebsites.net";
+// API_BASE_URL = "https://temptestedwin.azurewebsites.net";
 
 
 export default {
